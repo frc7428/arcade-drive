3399a4e7908f9cc5628aaee49240ea93ec3668cd
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-08-15 21:09:31 -0500

    create sample code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,141 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private DifferentialDrive mDrive;
+  private Joystick mJoystick;
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    mJoystick = new Joystick(0);
+
+    // Use either initializeTwoMotorDriveGroups or initializeOneMotorSides
+    initializeTwoMotorDriveGroups();
+  }
+
+  /**
+   * Use this method if there are two motors on each side.
+   */
+  private void initializeTwoMotorDriveGroups() {
+    var leftOne = new PWMVictorSPX(0);
+    var leftTwo = new PWMVictorSPX(2);
+    var rightOne = new PWMVictorSPX(1);
+    var rightTwo = new PWMVictorSPX(3);
+
+    var leftGroup = new SpeedControllerGroup(leftOne, leftTwo);
+    var rightGroup = new SpeedControllerGroup(rightOne, rightTwo);
+
+    mDrive = new DifferentialDrive(leftGroup, rightGroup);
+  }
+
+  /**
+   * Use this method if there is one motor on each side.
+   */
+  private void initializeOneMotorSides() {
+    var left = new PWMVictorSPX(0);
+    var right = new PWMVictorSPX(1);    
+    mDrive = new DifferentialDrive(left, right);
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  /**
+   * This function is called once when teleop is enabled.
+   */
+  @Override
+  public void teleopInit() {
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    mDrive.arcadeDrive(-mJoystick.getY(), mJoystick.getZ());
+
+    // You can use tankdrive instead if you have two joysticks or an xbox controller
+  }
+
+  /**
+   * This function is called once when the robot is disabled.
+   */
+  @Override
+  public void disabledInit() {
+  }
+
+  /**
+   * This function is called periodically when disabled.
+   */
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /**
+   * This function is called once when test mode is enabled.
+   */
+  @Override
+  public void testInit() {
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
