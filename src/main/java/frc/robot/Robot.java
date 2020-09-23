289adae5229dac004a87437fe1ed8390cef15efa
Gigawatts7428 <60040970+nightbomb2006@users.noreply.github.com>
2020-09-23 14:16:44 -0500

    Updated arcade drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,12 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+//import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+//import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -23,6 +27,17 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Robot extends TimedRobot {
   private DifferentialDrive mDrive;
   private Joystick mJoystick;
+  private SpeedController rightFront;
+  private SpeedController leftFront;
+  private SpeedController leftRear;
+  private SpeedController rightRear;
+  private SpeedControllerGroup right;
+  private SpeedControllerGroup left;
+  public static final int CAN_LEFT_FRONT_MOTOR_CONTROLLER = 1;
+  public static final int CAN_RIGHT_FRONT_MOTOR_CONTROLLER = 0;
+  public static final int CAN_LEFT_REAR_MOTOR_CONTROLLER = 2;
+  public static final int CAN_RIGHT_REAR_MOTOR_CONTROLLER = 3;
+
 
   /**
    * This function is run when the robot is first started up and should be
@@ -32,34 +47,38 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     mJoystick = new Joystick(0);
 
+    leftFront = new WPI_VictorSPX(CAN_LEFT_FRONT_MOTOR_CONTROLLER);
+    leftRear = new WPI_VictorSPX(CAN_LEFT_REAR_MOTOR_CONTROLLER);
+    left = new SpeedControllerGroup(leftFront, leftRear);
+    right = new SpeedControllerGroup(rightFront, rightRear);
+    rightFront = new WPI_VictorSPX(CAN_RIGHT_FRONT_MOTOR_CONTROLLER);
+    rightRear = new WPI_VictorSPX(CAN_RIGHT_REAR_MOTOR_CONTROLLER);
+    mDrive = new DifferentialDrive(left, right);
+   
+
     // Use either initializeTwoMotorDriveGroups or initializeOneMotorSides
-    initializeTwoMotorDriveGroups();
+    //initializeTwoMotorDriveGroups();
   }
 
   /**
    * Use this method if there are two motors on each side.
    */
-  private void initializeTwoMotorDriveGroups() {
-    var leftOne = new PWMVictorSPX(0);
-    var leftTwo = new PWMVictorSPX(2);
-    var rightOne = new PWMVictorSPX(1);
-    var rightTwo = new PWMVictorSPX(3);
+  //private void initializeTwoMotorDriveGroups() {
+    //var leftOne = new PWMVictorSPX(0);
+    //var leftTwo = new PWMVictorSPX(2);
+    //var rightOne = new PWMVictorSPX(1);
+    //var rightTwo = new PWMVictorSPX(3);
 
-    var leftGroup = new SpeedControllerGroup(leftOne, leftTwo);
-    var rightGroup = new SpeedControllerGroup(rightOne, rightTwo);
+    //var leftGroup = new SpeedControllerGroup(leftOne, leftTwo);
+    //var rightGroup = new SpeedControllerGroup(rightOne, rightTwo);
 
-    mDrive = new DifferentialDrive(leftGroup, rightGroup);
-  }
+    //mDrive = new DifferentialDrive(leftGroup, rightGroup);
+  
 
   /**
    * Use this method if there is one motor on each side.
    */
-  private void initializeOneMotorSides() {
-    var left = new PWMVictorSPX(0);
-    var right = new PWMVictorSPX(1);    
-    mDrive = new DifferentialDrive(left, right);
-  }
-
+  
   /**
    * This function is called every robot packet, no matter the mode. Use
    * this for items like diagnostics that you want ran during disabled,
@@ -99,6 +118,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopInit() {
+
   }
 
   /**
@@ -106,9 +126,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    mDrive.arcadeDrive(-mJoystick.getY(), mJoystick.getZ());
-
-    // You can use tankdrive instead if you have two joysticks or an xbox controller
+    mDrive.arcadeDrive(mJoystick.getRawAxis(0), -mJoystick.getRawAxis(1));
+    
+//       You can use tankdrive instead if you have two joysticks or an xbox controller
   }
 
   /**
